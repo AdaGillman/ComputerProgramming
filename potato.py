8928118e0082cefcc08ded32a8aa085a9b23622a
Scholar <scholar@paradigm.local>
2023-08-31 13:23:24 -0600

    character created

---
+++ potato.py
@@ -0,0 +1,30 @@
+person = "Vanessa"
+
+for i in range(10):
+	print (i)
+
+list = ['one', 'two', 'three']
+
+dictionary = {
+	'name': 'Vanessa',
+	'age': 16
+}
+
+def is_even_number (number):
+	if number % 2 == 0:
+		print('This is an even number!')
+	Else:print('This is an odd number!')
+
+is_even_number(43)
+
+MAIN_CHARACTER = "Mario"
+
+main_character = "Luigi"
+
+myName = "Mario"
+
+myNameIsNot = "Mario"
+
+my_name = "Bill"
+
+MyNameIs = "Bill"
