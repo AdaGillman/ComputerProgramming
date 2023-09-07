830c4c48e8d072f15e4ee65c14bfdb547c74c967
Scholar <scholar@paradigm.local>
2023-09-07 13:31:16 -0600

    intro to concepts

---
+++ Today.py
@@ -0,0 +1,41 @@
+my_name = "Ada"
+
+def give_a_name():
+    a_name = "Bill"
+    print(a_name)
+
+user_input = input("What is your name? ")
+print(f"Nice to meet you, {user_input}!")
+
+my_string = "This is my string."
+my_integer = 1234
+my_float = 3.14
+is_my_boolean = True
+
+print(my_float + my_float)
+
+my_declaration = ""
+
+add = 1 +2
+subtract = 2-1
+divide = 2/1
+multiply = 1*3
+modulo = 10%3
+exponent = 2 ** 3
+
+print(modulo)
+
+statment = 4 > 2
+statment2 = 4 >= 2
+statment3 = 4 <= 2
+statment4 = 4 != 2
+statment5 = 4 == 2
+
+print(statment5)
+
+
+message = 4 > 2 and 33 < 43
+message2 = 4 < 2 or 33 < 43
+x = False
+
+print(not x)
