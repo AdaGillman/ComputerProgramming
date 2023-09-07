59aa5394e5da782f569268f088aaca13e8dabb31
Scholar <scholar@paradigm.local>
2023-09-07 12:44:43 -0600

    created variables and a function

---
+++ Something.py
@@ -0,0 +1,11 @@
+my_name = "Ada Gillman"
+test_score = 12/12
+was_lunch_tasty = True
+Bday_classes = ["Art Around the world, ASL, Computer Programming, Yoga"]
+
+print (f"Hey my name is {my_name} I got {test_score} on my last ASL test. My classes today are {Bday_classes}")
+
+def add_two_numbers():
+    print(5+3)
+
+add_two_numbers()
