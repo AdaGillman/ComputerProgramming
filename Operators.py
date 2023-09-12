123a7d2baebcfc58b6fc03c563a41e4c9c3446b5
Scholar <scholar@paradigm.local>
2023-09-12 13:00:57 -0600

    intro to concepts

---
+++ Operators.py
@@ -0,0 +1,33 @@
+modulo = 3 % 9
+multiply = 3 * 9
+integerdivision = 11//3
+floatdivision = 11/3
+addition = 8 + 3
+subtraction = 8 - 3
+
+print(modulo)
+print(multiply)
+print(integerdivision)
+print(floatdivision)
+print(addition)
+print(subtraction)
+
+statment1 = 2 < 3
+statment2 = 6 > 4
+statment3 = 5 == 4
+statment4 = 6 >= 1
+statment5 = 8 <= 9
+statment6 = 8 != 2
+ 
+print(statment1)
+print(statment2)
+print(statment3)
+print(statment4)
+print(statment5)
+print(statment6)
+
+message1 = 7 > 2 and 6 < 7
+message2 = 7 < 2 or 6 < 7
+message3 = 5 > 7
+ 
+print(not message3)
