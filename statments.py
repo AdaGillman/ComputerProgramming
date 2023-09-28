167c7012a5eae5dd9be7df574ebef8a041b58c37
Scholar <scholar@paradigm.local>
2023-09-28 13:24:32 -0600

    if statments

--- statments.py
+++ statments.py
@@ -29,7 +29,59 @@ if num1 > num2:
 if num1 < num2:
     print(num2)
     print("is the higher number")
+
+if num1 == num2:
+    print("The numbers are equal")
     
-age1 = int(input("Give me the age of someone"))
-age2 = int(input("Give me the age of another person"))
-age3 = int(input("Now give me the age of one last person"))
+age1 = int(input("Give me the age of someone "))
+age2 = int(input("Give me the age of another person "))
+age3 = int(input("Now give me the age of one last person "))
+
+if age1 > age2 and age1 > age3:
+    print("The first person is the oldest:")
+    print(age1)
+
+if age2 > age1 and age2 > age3:
+    print("The second person is the oldest:")
+    print(age2)
+
+if age3 > age1 and age3 > age2:
+    print("The third person in the oldest:")
+    print(age3)
+
+if age1 == age2 and age1 == age3:
+    print("All three people are the same age")
+
+if age1 < age2 and age1 < age3:
+    print("The first person is the youngest:")
+    print(age1)
+
+if age2 < age1 and age2 < age3:
+    print("The second person is the youngest:")
+    print(age2)
+
+if age3 < age1 and age3 < age2:
+    print("The third person is the youngest:")
+    print(age3)
+
+class1 = int(input("How many classes were held? "))
+class2 = int(input("How many classes did you attend? "))
+
+if class2 > class1:
+    print("It doesn't work like that")
+
+class3 = (class2 / class1)
+class4 = (class3 * 100)
+
+print("You attended")
+print(class4)
+print("percent of the classes held")
+
+if class4 > 75 and class4 <= 100:
+    print("You are able to particapate in the test")
+
+if class4 < 75:
+    print("You cannot particapate in this test")
+
+if class4 > 100:
+    print("That doesn't really work")
