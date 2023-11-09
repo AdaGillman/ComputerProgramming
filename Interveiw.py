3eb21959cefa78b190cb3b403e90e43dbcc5afbe
Scholar <scholar@paradigm.local>
2023-11-09 12:53:28 -0700

    Interveiw

--- Interveiw.py
+++ Interveiw.py
@@ -18,9 +18,9 @@ print("Those languages are pretty useful thank you for answering.")
 
 question3 = int(input("How many years of experience do you have with programming? (Please answer in a number) "))
 
-while question3 < 3:
-    print("You need atleast 3 years of experience to get this job")
-    question3 = int(input("How many years of experience do you have with programming? "))
+if question3 < 3:
+    print("I'm sorry but in order to get this job you need atleast 3 years of experience. This interveiw is over thank you for coming.")
+    exit()
 
 question4 =  input("Do you have experience working in a team of developers or as an independent worker? ")
 
@@ -28,31 +28,27 @@ question5 = input("Would you be comfortable working with our team? ")
 
 question6 = input("Why would we want you to be a part of our company? ")
 
-question7 = int(input("What salary do you desire? "))
+question7 = (input("How often would you say you go on vacation? "))
+print("That sounds reasonable.")
 
-if question7 <= 20000:
+question8 = int(input("What salary do you desire? "))
+
+if question8 <= 20000:
     print("That is way to low and we can't accept you to have a salary of that amount.")
 
-if question7 >= 100000:
+if question8 >= 100000:
     print("That might be a little too big of a salary for starting out but maybe we could work up towards that.")
-else: 
-    print("That sounds like it could be reasonable.")
-
-question8 = int(input("How many days of the week would you be able to work?"))
-
-while question8 < 4:
-    print("If you want this job you would need to commit more time.")
-    question8 = int(input("How many days of the week would you be able to work? "))
-
-question9 = ("Are there any specific days you would not be able to work?")
-
-question10 = ("")
 
 
+question9 = int(input("How many days of the week are you able to work? (Please answer in a number) "))
 
+if question9 < 4:
+    print("If you want this job you would need to commit more time. Thank you for coming but this job isn't for you. ")
+    exit()
 
+question10 = input("Why do you want this job? ")
 
+print(f"Thank you for attending this interveiw {name}. I want to recap what we went over. Does this sound right?")
+reveiw = input(f"The position you are interested in, is {question1}, the languages you know are {question2}, you have {question3} years of experience in computer programming, you have worked as a(n) {question4} worker, you want to be a part of our company because {question6}, you go on vacation {question7}, your desired salary is {question8}, you are able to work {question9} days of the week, and lastly you want this job because {question10}. ")
 
-
-
-# print(f"Thank you for attending this interveiw {name}. I want to recap what we went over. Does this sound correct?")
+print("Thank you for coming to this interveiw, have a nice day.")
