23b2f0a1e1de6d6f11ebb99e06214918525112c5
AdaGillman <143127351+AdaGillman@users.noreply.github.com>
2023-12-09 16:43:49 -0700

    Update RPG.py
    Final RPG

--- RPG.py
+++ RPG.py
@@ -1,8 +1,8 @@
-inventory = []
+import random
 HP = 100
 Enemy_HP = 100
+inventory = []
 
-import random
 enemy_list = ["vicious yeti", "wild ice goblin", "giant frosty spider"]
 enemy = random.choice(enemy_list)
 
@@ -13,73 +13,62 @@ walk = ["1", "2", "3"]
 walk_chance = random.choice(walk)
 
 item = ["rope", "golden necklace", "picture of a dog"]
-name = input("Welcome fellow taveler! What is your name? ")
-print("Your adventure begins...")
-print(f"During your travels, you come across a snowy, ice-cold tundra, with no end in sight. What would you like to do {name}?")
-option1 = input("Press 'w' if you would like to walk, press 'p' if you would like to check your inventory. ")
-
-while option1 in "p":
-    option1 = input(f"{name}, you have {HP} HP and {inventory} in your inventory. Press 'w' to walk forward. ")
 
 def attack_enemy(HP, Enemy_HP):
-    while True:
-        if HP > 0:
-            your_attack = random.randint(10,100)
-            print(f"You attacked the enemy for {your_attack} damage!")
-            Enemy_HP -= your_attack
-
-        if Enemy_HP > 0:
-            enemy_attack = random.randint(10,130)
-            print(f"The enemy attacked you for {enemy_attack} damage!")
-            HP -= enemy_attack
-
-        if Enemy_HP <= 0:
-            print("You were able to deafeat the enemy!")
-            print("You received an item in your inventory!")
-            option1 = input("Press 'w' if you would like to walk, press 'p' if you would like to check your inventory. ")
-            inventory = [f"{item}"]
-            break
-
-        if HP <= 0:
-            print("You lived a great adventurous life, but sadly you died. May you forever be remembered.")
-            exit()
+    if HP > 0:
+        your_attack = random.randint(10,100)
+        print(f"You attacked the enemy for {your_attack} damage!")
+        Enemy_HP -= your_attack
+    return Enemy_HP
 
 def enemy_attacks_you(HP, Enemy_HP):
-    while True:
-        if Enemy_HP > 0:
-            enemy_attack = random.randint(10,100)
-            print(f"The enemy attacked you for {enemy_attack} damage!")
-            HP -= enemy_attack
-        
-        if HP > 0:
-            your_attack = random.randint(10,100)
-            print(f"You attacked the enemy for {your_attack} damage!")
-            Enemy_HP -= your_attack
+    if Enemy_HP > 0:
+        enemy_attack = random.randint(10,100)
+        print(f"The enemy attacked you for {enemy_attack} damage!")
+        HP -= enemy_attack
+    return HP
 
-        if Enemy_HP <= 0:
-            print("You were able to deafeat the enemy!")
-            print("You received an item in your inventory!")
-            option1 = input("Press 'w' if you would like to walk, press 'p' if you would like to check your inventory. ")
-            break
+name = input("Welcome taveler! What is your name? ")
+print("Your adventure begins...")
+print(f"During your travels, you come across a snowy, ice-cold tundra, with no end in sight. What would you like to do {name}?")
 
-        if HP <= 0:
-            print("You lived a great adventurous life, but sadly you died. May you forever be remembered.")
-            exit()
+while True:
+    option1 = input("Press 'w' if you would like to walk, press 'p' if you would like to check your inventory. ")
+    
+    if option1 == "p":
+        print(f"{name}, you have {HP} HP and {inventory} in your inventory.")
 
-while option1 == "w":
-        for enemy in enemy_list:
-            walk = ["1", "2", "3"]
-            walk_chance = random.choice(walk)
-            if walk_chance == "2":
-                print(f"A {enemy} appears!")
+    if option1 == "w":
+        walk_chance = random.choice(walk)
+        if walk_chance == "2":
+            enemy = random.choice(enemy_list)
+            print(f"A {enemy} appears!")
+            Enemy_HP = 100
+            while True:
                 fight_input = input("Would you like to attack or run? ")
                 if fight_input == "attack":
-                    attack_enemy(HP, Enemy_HP)
+                    Enemy_HP = attack_enemy(HP, Enemy_HP)
+                    if Enemy_HP <= 0:
+                        healing = random.randint(5,80)
+                        random_item = random.choice(item)
+                        print("You were able to deafeat the enemy!")
+                        print(f"You received {healing} health and a {random_item} in your inventory!")
+                        inventory.append(random_item)
+                        HP = min(100, HP + healing) 
+                        break
+
                 if fight_input == "run":
-                    if run == "1":
+                    chance = random.choice(run)
+                    if chance == "1":
                         print("It looks like you didn't escape.")
-                        enemy_attacks_you(HP, Enemy_HP)
+
                     else:
-                            print("You got away!")
-                            option1 = input("Press 'w' if you would like to walk, press 'p' if you would like to check your inventory. ")
-                            break
+                        print("You got away!")
+                        break
+
+                HP = enemy_attacks_you(HP, Enemy_HP)
+                if HP <= 0:
+                    print("You lived a great adventurous life, but sadly you died. May you forever be remembered.")
+                    exit()
+        else:
+            print("You walked forward.")
